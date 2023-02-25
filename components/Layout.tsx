3e85e6fff1dfa5d8b353baec2309b050599cf990
therealrinku <rinkunited2032@gmail.com>
2023-02-25 14:55:05 +0545

    dashboard and chart featfest

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,39 @@
-import { NextPage } from "next";
+import { Avatar, Tabs } from "antd";
 import React, { Fragment } from "react";
-import Sidebar from "./Sidebar";
+import { FiCreditCard, FiHome } from "react-icons/fi";
 
 export default function Layout({ children }: { children: React.ReactChild }) {
   return (
     <Fragment>
-      <Sidebar />
-      {children}
+      <div className="shadow text-white rounded-lg px-2 py-1 text-sm bg-gradient-to-r from-purple-500 to-pink-500 my-10 w-full max-w-3xl mx-auto">
+        <div className="bg-gradient-to-r from-purple-500 to-pink-500 sticky top-0">
+          <section className="m-2 flex items-center gap-2 justify-between">
+            <p className="font-bold">User109</p>
+
+            <div className="flex items-center gap-2">
+              <Avatar src="https://images.unsplash.com/photo-1669553640769-0e4019b67d46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1NnxTNE1LTEFzQkI3NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" />
+              <p className="font-bold">Rebecca Taylor</p>
+            </div>
+          </section>
+
+          <Tabs
+            items={[FiHome, FiCreditCard].map((Icon, i) => {
+              const id = String(i + 1);
+              return {
+                label: (
+                  <span className="flex items-center gap-2 text-white">
+                    <Icon />
+                    {i === 0 ? "Dashboard" : "Trade"}
+                  </span>
+                ),
+                key: id,
+              };
+            })}
+            centered
+          />
+        </div>
+        {children}
+      </div>
     </Fragment>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,96 @@
-import type { NextPage } from 'next'
-import { Fragment } from 'react'
+import { Avatar, Table, Tabs } from "antd";
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import { Fragment } from "react";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+const ReactApexChart = dynamic(() => import("react-apexcharts"),{ssr:false});
 
 const Home: NextPage = () => {
+  const series = [
+    {
+      name: "Desktops",
+      data: [10,20,30,40],
+    },
+  ];
+  const options: any = {
+    chart: {
+      type: "area",
+      zoom: {
+        enabled: false,
+      },
+      toolbar: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: "Weekly Asset Growth Chart",
+      align: "left",
+    },
+    xaxis: {
+      categories: ["Feb 20","Feb 19","Feb 18","Feb 17"],
+    },
+  };
+
+  const dataSource = [
+    {
+      key: "1",
+      name: "Coinbase IPO",
+      age: '$20',
+      count:200,
+      address: "$4000",
+    },
+    {
+      key: "1",
+      name: "Apex Stock LLC",
+      age: '$23',
+      count:200,
+      address: "$4600",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Asset Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "PPT",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Count",
+      dataIndex: "count",
+      key: "count",
+    },
+    {
+      title: "Total Value",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
   return (
     <Fragment>
-    </Fragment>
-  )
-}
+      <div className="mx-auto max-w-3xl">
+        <section className="bg-gradient-to-r from-pink-500 to-purple-500 rounded-lg p-5 mb-5 shadow">
+          <p className="text-lg font-bold mb-3">$50,050</p>
+          <p className="flex items-center text-red-100"><IoMdArrowDropdown size={17}/> 10 % down this week </p>
+        </section>
 
-export default Home
+        <ReactApexChart options={options} series={series} height="300" width="100%" />
+
+        <Table bordered dataSource={dataSource} columns={columns} pagination={false} />
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default Home;
